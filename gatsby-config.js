31973a6f08aad1e9d1e8b000b4043b9db502e795
Noel Bank <info@nbank.dev>
2020-11-07 15:11:04 +0100

    add inital sutff with gatsbyjs

---
+++ gatsby-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  // Since `gatsby-plugin-typescript` is automatically included in Gatsby you
+  // don't need to define it here (just if you need to change the options)
+  plugins: [
+    {
+      resolve: `gatsby-plugin-typescript`,
+      options: {
+        isTSX: true, // defaults to false
+        jsxPragma: `jsx`, // defaults to "React"
+        allExtensions: true, // defaults to false
+      },
+    },
+    `gatsby-plugin-sass`,
+  ],
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { PageProps } from 'gatsby';
+
+const Home: React.FC<PageProps> = () => (
+  <main>
+    <h1>developer preview</h1>
+    <p>for other site please visit them:</p>
+  </main>
+);
+
+export default Home;
